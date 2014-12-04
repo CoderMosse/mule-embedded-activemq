9c3f87b84545504eae0952c708281b0d4a6cf6ab
Simone Avossa <simone.avossa@mulesoft.com>
2014-12-04 14:59:10 +0000

    First commit

---
+++ embedded-activemq/src/main/java/com/mulesoft/messaging/PathExtractor.java
@@ -0,0 +1,18 @@
+package com.mulesoft.messaging;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PathExtractor {
+
+	private static final Logger logger = LoggerFactory.getLogger(PathExtractor.class);
+	
+	public String getAbsolutePath(String resource) throws IOException {
+		String absolutePath = this.getClass().getResource(resource).getPath();
+		logger.info("Set resource: " + absolutePath);
+		return absolutePath;
+	}
+	
+}
